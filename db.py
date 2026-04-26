e7886d585159ed33fba1bd735ed8a128d0279bef
SumanaGudivada <gudivadasumana@gmail.com>
2026-04-26 21:29:04 +0530

    Update db.py

--- db.py
+++ db.py
@@ -5,13 +5,20 @@ import os
 MONGO_URI = os.getenv("MONGO_URI")
 
 if not MONGO_URI:
-    raise ValueError("MONGO_URI not set")
+    raise ValueError("MONGO_URI not set. Please configure environment variable.")
 
 client = MongoClient(
     MONGO_URI,
     tlsCAFile=certifi.where(),
     serverSelectionTimeoutMS=5000
 )
+
+# Connection check
+try:
+    client.admin.command("ping")
+    print("MongoDB connected successfully")
+except Exception as e:
+    print("MongoDB connection failed:", e)
 
 db = client["rate_limiter_db"]
 collection = db["logs"]
